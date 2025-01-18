b2606c36a821b67b0a1a19fc3ad814b82504c1b8
tomixrm <chocolate0785lego@icloud.com>
2025-01-18 20:07:41 +0900

    abc330_eを書き直した。AC

--- prog_rust/CompetitiveProgramming/abc330_e/src/main.rs
+++ prog_rust/CompetitiveProgramming/abc330_e/src/main.rs
@@ -1,96 +1,56 @@
 // https://atcoder.jp/contests/abc330/tasks/abc330_e
-
 use proconio::input;
-use std::collections::BTreeMap;
-
-const DEBUG: bool = false;
-
-fn print_map(map: &BTreeMap<u128, u128>) {
-    for (_, (key, value)) in map.iter().enumerate() {
-        println!("#{}: count:{}", key, value);
-    }
-}
-
-fn update_map(map: &mut BTreeMap<u128, u128>, value: &u128, removed_value: &u128) {
-    if let Some(count) = map.get_mut(value) {
-        *count += 1;
-    } else {
-        map.entry(value.clone()).or_insert(1);
-    }
-
-    if let Some(count) = map.get_mut(removed_value) {
-        *count -= 1;
-        if *count == 0 {
-            map.remove(removed_value);
-        }
-    }
-}
-
-fn calc_mex(map: &BTreeMap<u128, u128>) {
-    let mut mex = u128::MAX;
-    let min_key = *map
-        .first_key_value()
-        .map(|(k, _)| k)
-        .expect("最小値が取り出せませんでした");
-    let max_key = *map
-        .last_key_value()
-        .map(|(k, _)| k)
-        .expect("最大値が取り出せませんでした");
-    if DEBUG {
-        println!("min:{} max:{} ", min_key, max_key);
-    }
-
-    if min_key == 1 {
-        mex = 0;
-    } else {
-        for k in (min_key + 1)..=(max_key + 1) {
-            if !map.contains_key(&k) {
-                mex = k;
-                break;
-            }
-        }
-    }
-    if DEBUG {
-        print!("mex:");
-    }
-    println!("{}", mex);
-}
+use std::collections::BTreeSet;
+use std::io::{self, Write};
 
 fn main() {
+    let stdout = io::stdout();
+    let mut out = io::BufWriter::new(stdout.lock());
+
     input! {
-        (n , q) : (u128, u128),
-    };
+        n: usize,
+        q: usize,
+        a: [usize; n],
+    }
 
-    let mut a_list = vec![0; n as usize];
-    let mut map: BTreeMap<u128, u128> = BTreeMap::new();
-
-    // 数列A1~Anの受け取り
-    for i in 0..n {
-        input! {
-            value :u128
+    let mut count: Vec<usize> = vec![0; n + 1];
+    for &val in &a {
+        if val <= n {
+            count[val] += 1;
         }
-        a_list[i as usize] = value;
-        //  数列Aの要素aについて、重複数を数える
-        update_map(&mut map, &value, &u128::MAX);
-        // calc_mex(&map);
-    }
-    if DEBUG {
-        println!("{:?}", a_list);
-        print_map(&map);
     }
 
+    let mut st: BTreeSet<usize> = BTreeSet::new();
+    for i in 0..=n {
+        if count[i] == 0 {
+            st.insert(i);
+        }
+    }
+
+    let mut a = a; 
     for _ in 0..q {
         input! {
-            (index, input_value) : (u128,u128),
-        };
-
-        let removed_value = a_list[index as usize - 1];
-        a_list[index as usize - 1] = input_value; // 数値の置き換え
-        update_map(&mut map, &input_value, &removed_value);
-        calc_mex(&map);
-        if DEBUG {
-            println!("{:?}", a_list);
-            print_map(&map);
+            i: usize,
+            x: usize,
         }
+        let i = i - 1;
+
+        let t = a[i];
+        if t <= n {
+            count[t] -= 1;
+            if count[t] == 0 {
+                st.insert(t);
+            }
+        }
+
+        a[i] = x;
+        if x <= n {
+            if count[x] == 0 {
+                st.remove(&x);
+            }
+            count[x] += 1;
+        }
+
+        writeln!(out, "{}", st.iter().next().unwrap()).unwrap();
     }
 }
