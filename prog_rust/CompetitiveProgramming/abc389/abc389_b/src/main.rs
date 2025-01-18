2fa0a69a8bf38df2734043ab7cbbd9e7f366f92b
tomixrm <chocolate0785lego@icloud.com>
2025-01-19 00:16:39 +0900

    abc398_bで再帰を使う方法

--- prog_rust/CompetitiveProgramming/abc389/abc389_b/src/main.rs
+++ prog_rust/CompetitiveProgramming/abc389/abc389_b/src/main.rs
@@ -1,23 +1,42 @@
 // https://atcoder.jp/contests/abc389/tasks/abc389_b
+
+
+fn fact(f: u128) -> u128 {
+    if f == 0 {
+        1
+    } else {
+        f.checked_mul(fact(f - 1)).unwrap_or(u128::MAX)
+    }
+}
+
 use proconio::input;
 fn main() {
     input!{
         x : u128,
     };
 
-    let mut val:u128 = 1;
-    let mut count = 0;
+    // let mut val:u128 = 1;
+    // let mut count = 0;
+    // loop{
+    //     count += 1;
+    //     val *= count;
+        
+    //     if val == x {
+    //         println!("{}",count);
+    //         break;
+    //     }
+    //     // println!("count:{} val:{} ",count,val);
+    //     if val > x {
+    //         println!("なし！");
+    //         break;
+    //     }
+    // }
+
+    let mut count: u128 = 0;
     loop{
         count += 1;
-        val *= count;
-        
-        if val == x {
+        if fact(count) == x {
             println!("{}",count);
-            break;
-        }
-        // println!("count:{} val:{} ",count,val);
-        if val > x {
-            println!("なし！");
             break;
         }
     }
